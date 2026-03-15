eff4a163255f2ac4d1a07b279427d47bbafb235f
Sachith Dassanayaka <sachitheranga.1990@gmail.com>
2026-03-15 00:19:19 -0400

    updaed the process.py

--- src/process.py
+++ src/process.py
@@ -1,3 +1,17 @@
-def clean_text(text: str) -> str:
-    text = text.lower().strip()
+import pandas as pd
+import re
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
     return text
+
+
+def load_and_preprocess(path):
+
+    df = pd.read_csv(path)
+
+    df["clean_text"] = df["text"].apply(clean_text)
+
+    return df
