3a997434b67589b40227e3d164fb500eb4901e2d
Sachith Dassanayaka <sachitheranga.1990@gmail.com>
2026-03-15 00:42:52 -0400

    update train.py

--- src/train.py
+++ src/train.py
@@ -1,5 +1,31 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+from preprocess import load_and_preprocess
+
+
 def train_model():
-    print("Training pipeline placeholder")
+
+    df = load_and_preprocess("data/sample/reviews_sample.csv")
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        df["clean_text"], df["label"], test_size=0.2, random_state=42
+    )
+
+    vectorizer = TfidfVectorizer(max_features=5000)
+
+    X_train_vec = vectorizer.fit_transform(X_train)
+
+    model = LogisticRegression()
+
+    model.fit(X_train_vec, y_train)
+
+    joblib.dump(model, "models/model.pkl")
+    joblib.dump(vectorizer, "models/vectorizer.pkl")
+
 
 if __name__ == "__main__":
     train_model()
