a0536eb541616254a741a6303095949265ca63ee
Sachith Dassanayaka <sachitheranga.1990@gmail.com>
2026-03-15 00:45:36 -0400

    update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,2 +1,14 @@
-def predict(text: str):
-    return {"text": text, "prediction": "placeholder"}
+import joblib
+
+
+model = joblib.load("models/model.pkl")
+vectorizer = joblib.load("models/vectorizer.pkl")
+
+
+def predict(text):
+
+    vec = vectorizer.transform([text])
+
+    prediction = model.predict(vec)[0]
+
+    return prediction
