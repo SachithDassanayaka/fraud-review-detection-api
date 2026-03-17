61d423cef70f49223c1ee13386f7a00adb64539d
Sachith Dassanayaka <sachitheranga.1990@gmail.com>
2026-03-16 22:58:33 -0400

    update main.py for fastAPI

--- api/main.py
+++ api/main.py
@@ -1,7 +1,33 @@
+import os
+import sys
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# allow importing from src/
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+REPO_ROOT = os.path.dirname(CURRENT_DIR)
+SRC_DIR = os.path.join(REPO_ROOT, "src")
+
+if SRC_DIR not in sys.path:
+    sys.path.append(SRC_DIR)
+
+from predict import predict
+
 
 app = FastAPI(title="Fraud Review Detection API")
 
+
+class ReviewRequest(BaseModel):
+    text: str
+
+
 @app.get("/")
 def root():
-    return {"message": "Fraud Review Detection API running"}
+    return {"message": "Fraud Review Detection API is running"}
+
+
+@app.post("/predict")
+def predict_review(request: ReviewRequest):
+    result = predict(request.text)
+    return result
